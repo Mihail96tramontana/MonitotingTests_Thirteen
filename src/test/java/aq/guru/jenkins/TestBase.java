27a68488f2b78a8691356923caed404f64f86cd6
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2022-07-01 16:19:06 +0300

    test

--- src/test/java/aq/guru/jenkins/TestBase.java
+++ src/test/java/aq/guru/jenkins/TestBase.java
@@ -2,7 +2,9 @@ package aq.guru.jenkins;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -23,5 +25,13 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true); //свойство с ключом для передачи видео в формате VNC
         capabilities.setCapability("enableVideo", true); //свойство для записи видео
         Configuration.browserCapabilities = capabilities;
+        }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
