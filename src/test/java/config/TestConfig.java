dd07288d3cf6276cb9757a8a38cba0fcbfc7e17a
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2022-07-06 22:18:48 +0300

    test

--- src/test/java/config/TestConfig.java
+++ src/test/java/config/TestConfig.java
@@ -2,7 +2,7 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources("classpath:config/credentials.properties")
+@Config.Sources("classpath:config/credentials.properties_examples")
     public interface TestConfig extends Config {
         String login();
         String password();
